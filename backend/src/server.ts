c151c19b6e3579c19cfb5bdb3cc10657eda24c5d
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2026-06-02 11:20:39 +0700

    feat: use new frontend url

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://bnsp-frontend.vercel.app"],
+    origin: ["http://localhost:5173", "https://bnsp.barrsan.dev"],
   }),
 );
 
